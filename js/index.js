dae2909ecae8b64911c0cb54c23277df73b02686
harr0627 <harr0627@algonquinlive.com>
2021-09-30 22:20:03 -0400

    updated style

--- js/index.js
+++ js/index.js
@@ -10,23 +10,26 @@ const APP = {
     document
       .getElementById('useLocation')
       .addEventListener('click', APP.getLocation);
+    document
+      .getElementById('useLocation')
+      .addEventListener('click', APP.getLocation);
     document.querySelectorAll('.nav-link').forEach((link) => {
       link.addEventListener('click', APP.weatherNav);
     });
+    APP.lastWeather();
   },
   searchLocation: async function (ev) {
     ev.preventDefault();
     let query = document.getElementById('city').value.trim();
+    let location = document.querySelector('.location');
+
     if (!query) return false;
     let coord = await getGeolocation(query);
-    // await locationStorage({ coord });
     let reverse = await reverseLocation(coord.lat, coord.lon);
-    let location = document.querySelector('.location');
     location.innerHTML = `${reverse.address.city}, ${reverse.address.state} ${reverse.address.country}`;
     let forecast = await getForecast({ coord });
     await APP.showForecast({ forecast });
     await forecastStorage({ forecast });
-    console.log(forecast);
   },
   getLocation: async function (ev) {
     let options = {
@@ -37,85 +40,100 @@ const APP = {
     navigator.geolocation.getCurrentPosition(APP.ftw, APP.wtf, options);
   },
   ftw: async function (position) {
+    //position found
     let reverse = await reverseLocation(
       position.coords.latitude.toFixed(2),
       position.coords.longitude.toFixed(2)
     );
-    console.log({ reverse });
     let location = document.querySelector('.location');
     location.innerHTML = `${reverse.address.city}, ${reverse.address.state} ${reverse.address.country}`;
     await locationStorage({ reverse });
     let forecast = await getForecast('metric', { reverse });
     await forecastStorage({ forecast });
     await APP.showForecast({ forecast });
-    console.log(forecast);
   },
   wtf: (err) => {
-    //geolocation failed
+    //location failed
     console.error(err);
   },
   showForecast: async function (data) {
     let mainCard = document.querySelector('.mainCard');
     let cards = document.querySelector('.cards');
-    let hourly = document.querySelector('#hourly');
-    let weekly = document.querySelector('#weekly');
-    mainCard.innerHTML = data.forecast.daily
-      .map((day, idx) => {
-        if (idx == 0) {
-          let dt = new Date(day.dt * 1000); //timestamp * 1000
-          let imageId = day.weather[0].icon;
-          let image = createWeatherIcon(imageId);
+    let hourly = document.querySelector('.hourly');
+    let weekly = document.querySelector('.weekly');
+    let tzn = data.forecast.timezone;
+    let current = data.forecast.current;
+    console.log(current);
+    let dt = new Date(current.dt * 1000);
+    let imageId = current.weather[0].icon;
+    let image = createWeatherIcon(imageId);
 
-          return `<div class="card" style="width: 99%">
-          <div class="row no-gutters">
-              <div class="col center" >
-                  <img class="align-middle" src="${image}@4x.png" class="img-fluid" alt="${
-            day.weather[0].description
-          }">
+    mainCard.innerHTML = `<div class="card main-card">
+              <div class="col card-image" >
+                  <img class="" src="${image}@4x.png" class="img-fluid" alt="${
+      current.weather[0].description
+    }">
               </div>
               <div class="col">
                   <div class="card-block px-2">
-                      <h4 class="card-title center">${dt.toDateString()}</h4>
-                      <p class="card-text">High ${day.temp.max}&deg;C </p>
-                      <p class="card-text">Low ${day.temp.min}&deg;C </p>
-                        <p class="card-text">High Feels like ${
-                          day.feels_like.day
-                        }&deg;C</p>
-                        <p class="card-text">Humidity ${day.humidity}%</p>
-                        <p class="card-text">UV Index ${day.uvi}</p>
-                        <p class="card-text">Precipitation ${day.pop * 100}%</p>
+                      <h4 class="card-title main">${dt.toLocaleDateString(
+                        navigator.language,
+                        {
+                          timeZone: tzn,
+                          dateStyle: 'long',
+                        }
+                      )}</h4>
+                      <p class="card-text current">Current: ${parseInt(
+                        current.temp
+                      )}&deg;C </p>
+                      <p class="card-text">Feels Like: ${parseInt(
+                        current.feels_like
+                      )}&deg;C </p>
+                      <p class="card-text">Humidity: ${parseInt(
+                        current.humidity
+                      )}&deg;C </p>
+                      <p class="card-text">UV Index: ${parseInt(
+                        current.uvi
+                      )}&deg;C </p>
                   </div>
               </div>
           </div>
     </div>`;
-        }
-      })
-      .join(' ');
     cards.innerHTML = data.forecast.daily
       .map((day, idx) => {
         if (idx > 0 && idx <= 2) {
-          let dt = new Date(day.dt * 1000); //timestamp * 1000
+          let dt = new Date(day.dt * 1000);
           let imageId = day.weather[0].icon;
           let image = createWeatherIcon(imageId);
-          return `<div class="col-lg-2" style="display:inline-block; width:49%; margin:0;">
-          <div class="card" >
-          <div class="row no-gutters">
-              <div class="col center" >
-                  <img class="align-middle" src="${image}@4x.png" class="img-fluid" alt="${
+          return `<div class="card second-cards">
+              <div class="col card-image">
+                  <img class="align-middle" src="${image}@2x.png" class="img-fluid" alt="${
             day.weather[0].description
           }">
               </div>
               <div class="col">
-                  <div class="card-block px-2">
-                      <h4 class="card-title center">${dt.toDateString()}</h4>
-                      <p class="card-text">High ${day.temp.max}&deg;C </p>
-                      <p class="card-text">Low ${day.temp.min}&deg;C </p>
-                        <p class="card-text">High Feels like ${
+                  <div class="card-block">
+                      <h4 class="card-title">${dt.toLocaleDateString(
+                        navigator.language,
+                        {
+                          timeZone: tzn,
+                          dateStyle: 'long',
+                        }
+                      )}</h4>
+                      <div class="weather-stats">
+                      <p class="card-text">High ${parseInt(
+                        day.temp.max
+                      )}&deg;C </p>
+                      <p class="card-text">Low ${parseInt(
+                        day.temp.min
+                      )}&deg;C </p>
+                        <p class="card-text">High Feels like ${parseInt(
                           day.feels_like.day
-                        }&deg;C</p>
-                        <p class="card-text">Precipitation ${day.pop * 100}%</p>
-                  </div>
-              </div>
+                        )}&deg;C</p>
+                        <p class="card-text">Precipitation ${parseInt(
+                          day.pop * 100
+                        )}%</p>
+                        </div>
           </div>
         </div>
     </div>`;
@@ -125,34 +143,37 @@ const APP = {
     hourly.innerHTML = data.forecast.hourly
       .map((hour, idx) => {
         if (idx <= 5) {
-          let dt = new Date(hour.dt * 1000); //timestamp * 1000
+          let dt = new Date(hour.dt * 1000);
           let imageId = hour.weather[0].icon;
           let image = createWeatherIcon(imageId);
-          return `<div class="col-lg-2" style="display:inline-block;  margin:0;">
-        <div class="card" style="width:100%;">
-        <div class="row no-gutters">
-            <div class="col center" >
+          return `<div class="card small-cards">
+            <div class="col card-image" >
                 <img class="align-middle" src="${image}@2x.png" class="img-fluid" alt="${
             hour.weather[0].description
           }">
             </div>
             <div class="col">
-                <div class="card-block px-2">
-                    <h4 class="card-title center">${dt.toLocaleTimeString(
+                <div class="card-block">
+                    <h4 class="card-title">${dt.toLocaleTimeString(
                       navigator.language,
                       {
+                        timeZone: tzn,
                         hour: 'numeric',
                       }
                     )}</h4>
-                    <p class="card-text">Currently ${hour.temp}&deg;C </p>
-                    <p class="card-text">Feels like ${
+                    <p class="card-text">Currently ${parseInt(
+                      hour.temp
+                    )}&deg;C </p>
+                    <p class="card-text">Feels like ${parseInt(
                       hour.feels_like
-                    }&deg;C </p>
-                      <p class="card-text">Precipitation ${hour.pop * 100}%</p>
-                      <p class="card-text">Humidity ${hour.humidity}%</p>
+                    )}&deg;C </p>
+                      <p class="card-text">Precipitation ${parseInt(
+                        hour.pop * 100
+                      )}%</p>
+                      <p class="card-text">Humidity ${parseInt(
+                        hour.humidity
+                      )}%</p>
                 </div>
-            </div>
-        </div>
       </div>
   </div>`;
         }
@@ -161,25 +182,33 @@ const APP = {
     weekly.innerHTML = data.forecast.daily
       .map((day, idx) => {
         if (idx <= 5) {
-          let dt = new Date(day.dt * 1000); //timestamp * 1000
+          let dt = new Date(day.dt * 1000);
           let imageId = day.weather[0].icon;
           let image = createWeatherIcon(imageId);
-          return `<div class="col" style="display:inline-block; width:100%; margin:0;">
-        <div class="card" style="width:100%;">
-        <div class="row no-gutters">
-            <div class="col center" >
+          return `<div class="card small-cards">
+            <div class="col card-image" >
                 <img class="align-middle" src="${image}@2x.png" class="img-fluid" alt="${
             day.weather[0].description
           }">
             </div>
             <div class="col">
-                <div class="card-block px-2">
-                    <h4 class="card-title center">${dt.toDateString()}</h4>
-                    <p class="card-text">High ${day.temp.max}&deg;C </p>
-                    <p class="card-text">Low ${day.temp.min}&deg;C </p>
-                      <p class="card-text">Precipitation ${day.pop * 100}%</p>
-                </div>
-            </div>
+                <div class="card-block">
+                    <h4 class="card-title center">${dt.toLocaleDateString(
+                      navigator.language,
+                      {
+                        timeZone: tzn,
+                        dateStyle: 'long',
+                      }
+                    )}</h4>
+                    <p class="card-text">High ${parseInt(
+                      day.temp.max
+                    )}&deg;C </p>
+                    <p class="card-text">Low ${parseInt(
+                      day.temp.min
+                    )}&deg;C </p>
+                      <p class="card-text">Precipitation ${parseInt(
+                        day.pop * 100
+                      )}%</p>
         </div>
       </div>
   </div>`;
@@ -192,6 +221,16 @@ const APP = {
     let current = ev.target.getAttribute('data-target');
     document.querySelector('.active').classList.remove('active');
     document.getElementById(current).classList.add('active');
+  },
+  lastWeather: function () {
+    if (localStorage.getItem('forecast') && localStorage.getItem('location')) {
+      let locationData = JSON.parse(localStorage.getItem('location'));
+      let forecastData = JSON.parse(localStorage.getItem('forecast'));
+      let location = document.querySelector('.location');
+      location.innerHTML = `${locationData.reverse.address.city}, ${locationData.reverse.address.state} ${locationData.reverse.address.country}`;
+      //use location from storage to get data
+      APP.showForecast(forecastData);
+    }
   },
 };
 
